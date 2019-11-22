e43999d9b1ad4fbe9712452007244c9bb9366d8b
AlexeyBondarenko <bondarenko.alex.sergeevich@gmail.com>
2019-11-22 19:43:53 +0200

    Make factorial

---
+++ assets/js/task1.js
@@ -0,0 +1,53 @@
+function f() {
+    
+}
+
+
+
+
+
+
+/*function factorial(n) {
+
+    return n != 1 ? n * factorial(n - 1) : 1;
+
+
+}*/
+
+
+/*
+function pow(x, n) {
+    let result = x;
+
+    for (let i = 1; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+
+if (n < 1) {
+    alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+} else {
+    alert( pow(x, n) );
+}
+*/
+
+
+
+
+
+
+
+
+
+/*
+
+function min(a, b) {
+    return a < b ? a : b;
+    }
+*/
+
