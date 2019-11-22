0b0122a9fa650ff78d6d6c9e34efd4df804532ca
AlexeyBondarenko <bondarenko.alex.sergeevich@gmail.com>
2019-11-22 20:43:52 +0200

    Solve task 1

--- assets/js/task1.js
+++ assets/js/task1.js
@@ -1,18 +1,54 @@
-function f() {
-    
+'use strict';
+
+/*
+2. Написать функцию, которая выводит все числа из заданного
+пользователем диапазона в прямом порядке. И еще одну
+функцию – для вывода в обратном порядке.
+ */
+
+
+function logRangeR(from, to) {
+
+    if (from < to) {
+
+        console.log(from);
+
+        logRangeR(from + 1, to);
+
+
+    }
+
+}
+
+function logRangeReverse(from, to) {
+
+    if (from < to) {
+
+        console.log(to);
+
+        logRangeReverse(from, to - 1);
+
+
+    }
+
+}
+
+function logRange(from, to) {
+
+    for (let i = from; i < to; i++) {
+        console.log(i);
+    }
+
+
 }
 
 
+function factorial(n) {
+
+    return n !== 1 ? n * factorial(n - 1) : 1;
 
 
-
-
-/*function factorial(n) {
-
-    return n != 1 ? n * factorial(n - 1) : 1;
-
-
-}*/
+}
 
 
 /*
@@ -37,17 +73,39 @@ if (n < 1) {
 */
 
 
-
-
-
-
-
-
-
 /*
 
 function min(a, b) {
     return a < b ? a : b;
     }
 */
+
+/*
+3. Написать функцию, которая выводит переданное ей число
+задом наперед.
+Например: число 1234 вывести как 4321.
+ */
+function getMirrorNumber(number) {
+
+    if (number < 10) {
+        return number;
+    }
+
+    const lastDigit = number % 10;
+
+    return +`${lastDigit}${getMirrorNumber((number - lastDigit) / 10)}`;
+
+}
+
+function getDigitSumOfNumber(number) {
+
+    if (number < 10) {
+        return number;
+    }
+
+    const lastDigit = number % 10;
+
+    return lastDigit + getDigitSumOfNumber((number - lastDigit) / 10);
+
+}
 
